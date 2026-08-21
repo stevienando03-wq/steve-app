874fa5cd5e793318b2d38833cacc55efe11d0e57
Steve <stevie.nando03@gmail.com>
2026-08-21 12:42:14 +0200

    Calendrier : bouton dedie pour telecharger les rendez-vous seuls avec leurs trois rappels, l import Google les conserve contrairement a l abonnement

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "steve-v11";
+const CACHE = "steve-v12";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./icon-192.png", "./icon-512.png", "./icon-180.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
