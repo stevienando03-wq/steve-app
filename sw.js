91f1c20932896b469188df9b3de2072392997a89
Steve <stevie.nando03@gmail.com>
2026-09-21 18:56:18 +0200

    Chiffres du marche recomptes sur le vrai texte des annonces, v54
    Le comptage du 13/09 lisait les 468 fichiers L'offre.txt des dossiers de
    candidature. Ces fichiers ne contiennent pas l'annonce mais le dossier
    fabrique autour : la ligne de themes generee et le message LinkedIn type.
    Le mot reporting y sortait a 100 % a cause du modele de message.
    
    Refait sur 206 annonces APEC avec leur texte integral et les competences
    cochees par le recruteur, plus 203 annonces etrangeres de sept marches.
    
    Ce qui change :
    - DipIFR RETIRE de la tete de liste. Zero mention sur 409 annonces.
    - Power BI passe devant SQL, 19 contre 14, l inverse de ce qui etait ecrit.
    - Consolidation 31 sur 206, et non 152 sur 468.
    - DSCG 18 sur 206 et non 4, mais concentre sur les postes d entree.
    - AMF et TOEIC recales sur la nouvelle base.
    - Dimension etrangere : ACCA 86 % a Dublin, Power BI 52 % a Montreal.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "steve-v53";
+const CACHE = "steve-v54";
 const ASSETS = ["./", "./index.html", "./fiches.js", "./actu.js", "./entrainement.js", "./dscg.js", "./manifest.webmanifest", "./icon.svg", "./icon-192.png", "./icon-512.png", "./icon-180.png"];
 const DONNEES = ["fiches.js", "actu.js", "entrainement.js", "dscg.js"];
 
